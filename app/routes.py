e83dd4d7b0965b3934191cb3c27f3da5d6abecb7
Harry Sunaryo <Harrysunaryo03@gmail.com>
2019-01-08 10:12:19 +0800

    add route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import api
-from app.handler import HelloWorldHandler,PerkalianHandler,DemoCrudHandler
+from app.handler import HelloWorldHandler,PerkalianHandler,DemoCrudHandler,PembeliHandler,ProdukHandler
 
 api.add_resource(HelloWorldHandler,'/')
 api.add_resource(PerkalianHandler,'/kali','/kali/<int:bil1>/<int:bil2>')
 api.add_resource(DemoCrudHandler,'/demo','/demo/<int:id>')
+api.add_resource(PembeliHandler,'/pembeli','/pembeli/<int:id>')
+api.add_resource(ProdukHandler,'/produk','/produk/<int:id>')
